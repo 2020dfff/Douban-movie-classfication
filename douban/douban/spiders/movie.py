2ab4f5a895dece79b76e4ae3ffc84b8ffc7e3b82
p0ny <p0uy@outlook.com>
2022-06-17 19:50:42 +0800

    Update movie.py

--- douban/douban/spiders/movie.py
+++ douban/douban/spiders/movie.py
@@ -7,22 +7,18 @@ class MovieSpider(scrapy.Spider):
     start_urls = ['https://movie.douban.com/top250']
 
     def parse(self, response):
-        li = response.css('.grid_view li')
-        pass
-
-    def parse(self, response):
         item = DoubanMovie()
         for node in response.xpath('//ol[@class="grid_view"]/li'):
             item['ranking'] = node.xpath('.//div[@class="pic"]/em/text()').extract()[0]
             item['name'] = node.xpath('.//span[@class="title"][1]/text()').extract()[0]
             item['score'] = node.xpath('.//div[@class="star"]/span[2]/text()').extract()[0]
             item['review_count'] = node.xpath('.//div[@class="star"]/span[4]/text()').extract()[0]
-            item['quote'] = node.xpath('.//p[@class="quote"]/span/text()').extract_first()#有一项没有quote所以用first
+            item['quote'] = node.xpath('.//p[@class="quote"]/span/text()').extract_first()
             item['detail_url'] = node.xpath('.//div[@class="pic"]/a/@href').extract()[0]
             item['introduce'] = ''.join(node.xpath('.//div[@class="bd"]/p/text()').extract()[0]).strip() + '\n' + ''.join(node.xpath('.//div[@class="bd"]/p[1]/text()').extract()[1]).strip()
 
             yield item
-            print(item)#看看输出
+            print(item)
 
         next_page = response.xpath('//span[@class="next"]/a/@href')
         if next_page:
